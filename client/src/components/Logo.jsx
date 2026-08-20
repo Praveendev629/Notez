948e53ca0ff491bec851395cea1d6f10bbc69d02
Praveendev629 <praveenseenu629@gmail.com>
2026-08-20 14:03:29 +0530

    1234

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export function LogoMark({ className = '', size = 200 }) {
   return (
     <img
-      src="/assets/imag1.jpg"
+      src="/assets/imag1.png"
       alt="Notez logo mark"
       width={size}
       height={size}
@@ -14,7 +14,7 @@ export function LogoMark({ className = '', size = 200 }) {
 export function LogoText({ className = '', size = 360 }) {
   return (
     <img
-      src="/assets/logo.jpg"
+      src="/assets/logo.png"
       alt="Notez"
       width={size}
       height={size * (120 / 420)}
